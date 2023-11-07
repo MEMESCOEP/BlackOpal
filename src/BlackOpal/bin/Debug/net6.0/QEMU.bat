2bb357acf6b1a0913c8ef9283ac2b08ebf499f03
Andrew Maney <xxxmemescoepxxx@gmail.com>
2023-11-07 07:30:12 -0500

    Update cdrom name in QEMU.bat

--- src/BlackOpal/bin/Debug/net6.0/QEMU.bat
+++ src/BlackOpal/bin/Debug/net6.0/QEMU.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 echo Starting QEMU...
-"C:\Program Files\qemu\qemu-system-x86_64.exe" -cdrom "CosmosOS-Learning.iso" -m 128
+"C:\Program Files\qemu\qemu-system-x86_64.exe" -cdrom "BlackOpal.iso" -m 128
